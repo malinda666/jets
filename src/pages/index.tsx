9330964e3692fd077cc9be4d45cc0915e6c651ed
Malinda Anupama <55244908+malinda666@users.noreply.github.com>
2022-08-29 18:23:13 +0530

    aded title comp

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import type { NextPage } from 'next'
-import { MutableRefObject, useEffect, useRef } from 'react'
 import gsap from 'gsap'
 import ScrollTrigger from 'gsap/dist/ScrollTrigger'
 
@@ -8,30 +7,10 @@ import { Section } from '@/components'
 gsap.registerPlugin(ScrollTrigger)
 
 const Home: NextPage = () => {
-  const headingRef = useRef() as MutableRefObject<HTMLHeadingElement>
-
-  useEffect(() => {
-    const trigger = gsap.utils.selector(document)('section')[0]
-    gsap.to(headingRef.current, {
-      scale: 3,
-      ease: 'none',
-      duration: 1,
-      scrollTrigger: {
-        trigger: trigger,
-        markers: true,
-        start: 'center center',
-        end: '+=300',
-        scrub: 1,
-      },
-    })
-  }, [])
-
   return (
     <>
       <Section>
-        <h1 className="text-9xl" ref={headingRef}>
-          First section
-        </h1>
+        <h1 className="text-9xl">First section</h1>
       </Section>
       <Section>
         <h1 className="text-9xl">Second section</h1>
